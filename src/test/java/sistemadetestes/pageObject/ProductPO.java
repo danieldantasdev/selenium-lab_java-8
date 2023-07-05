1ebd78a8593394543c74f51cdf90a83a2a9b7a92
danieldantasdev <contatolobinhodev@gmail.com>
2023-07-05 20:21:46 -0300

    feat: implements five new tests in use case product

--- src/test/java/sistemadetestes/pageObject/ProductPO.java
+++ src/test/java/sistemadetestes/pageObject/ProductPO.java
@@ -1,5 +1,6 @@
 package sistemadetestes.pageObject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -34,6 +35,21 @@ public class ProductPO extends BasePO {
 
     @FindBy(css = "div.alert.alert-danger")
     public WebElement spamErro;
+
+    @FindBy(css = "div.card")
+    public WebElement divDetalhes;
+
+    @FindBy(id = "detalhes-nome")
+    public WebElement spanNomeProdutoDetalhes;
+
+    @FindBy(id = "detalhes-quantidade")
+    public WebElement spanQuantidadeProdutoDetalhes;
+
+    @FindBy(id = "detalhes-valor")
+    public WebElement spanValorProdutoDetalhes;
+
+    @FindBy(id = "detalhes-data")
+    public WebElement spanDataCadastroProdutoDetalhes;
 
     /**
      * Construtor da classe.
@@ -115,4 +131,263 @@ public class ProductPO extends BasePO {
         return driver.getTitle();
     }
 
+    /**
+     * Limpa o campo de nome do produto.
+     */
+    public void limparCampoNome() {
+        WebElement campoNome = driver.findElement(By.id("nome"));
+        campoNome.clear();
+    }
+
+    /**
+     * Preenche o campo de nome do produto com o valor especificado.
+     *
+     * @param codigo O nome do produto.
+     */
+    public void preencherCampoCodigo(String codigo) {
+        WebElement campoCodigo = driver.findElement(By.id("codigo"));
+        campoCodigo.sendKeys(codigo);
+    }
+
+    /**
+     * Preenche o campo de nome do produto com o valor especificado.
+     *
+     * @param nome O nome do produto.
+     */
+    public void preencherCampoNome(String nome) {
+        WebElement campoNome = driver.findElement(By.id("nome"));
+        campoNome.sendKeys(nome);
+    }
+
+    /**
+     * Preenche o campo de quantidade do produto com o valor especificado.
+     *
+     * @param quantidade A quantidade do produto.
+     */
+    public void preencherCampoQuantidade(String quantidade) {
+        WebElement campoQuantidade = driver.findElement(By.id("quantidade"));
+        campoQuantidade.sendKeys(quantidade);
+    }
+
+    /**
+     * Preenche o campo de valor do produto com o valor especificado.
+     *
+     * @param valor O valor do produto.
+     */
+    public void preencherCampoValor(String valor) {
+        WebElement campoValor = driver.findElement(By.id("valor"));
+        campoValor.sendKeys(valor);
+    }
+
+    /**
+     * Preenche o campo de data de cadastro do produto com o valor especificado.
+     *
+     * @param data A data de cadastro do produto.
+     */
+    public void preencherCampoDataCadastro(String data) {
+        WebElement campoDataCadastro = driver.findElement(By.id("data"));
+        campoDataCadastro.sendKeys(data);
+    }
+
+    /**
+     * Obtém o texto da mensagem de erro.
+     *
+     * @return O texto da mensagem de erro.
+     */
+    public String obterTextoMensagemErro() {
+        return spamErro.getText();
+    }
+
+    /**
+     * Obtém o texto da última linha da tabela de produtos.
+     *
+     * @return O texto da última linha da tabela.
+     */
+    public String obterTextoUltimaLinhaTabela() {
+        List<WebElement> rows = tableProduto;
+        if (!rows.isEmpty()) {
+            WebElement lastRow = rows.get(rows.size() - 1);
+            return lastRow.getText();
+        }
+        return "";
+    }
+
+    /**
+     * Verifica se um produto com o nome especificado está presente na tabela de produtos.
+     *
+     * @param nome O nome do produto.
+     * @return true se o produto estiver presente, false caso contrário.
+     */
+    public boolean verificarProdutoNaTabela(String nome) {
+        List<WebElement> rows = tableProduto;
+        for (WebElement row : rows) {
+            if (row.getText().contains(nome)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Clica no botão de edição (ícone de lápis) do último produto da tabela.
+     */
+    public void clicarBotaoEdicaoUltimoProduto() {
+        List<WebElement> rows = tableProduto;
+        if (!rows.isEmpty()) {
+            WebElement lastRow = rows.get(rows.size() - 1);
+            WebElement buttonEdit = lastRow.findElement(By.className("btn-edit"));
+            buttonEdit.click();
+        }
+    }
+
+    /**
+     * Preenche o campo de nome do produto na tela de edição com o valor especificado.
+     *
+     * @param nome O novo nome do produto.
+     */
+    public void preencherCampoNomeEdicao(String nome) {
+        WebElement campoNome = driver.findElement(By.id("edit-nome"));
+        campoNome.clear();
+        campoNome.sendKeys(nome);
+    }
+
+    /**
+     * Preenche o campo de quantidade do produto na tela de edição com o valor especificado.
+     *
+     * @param quantidade A nova quantidade do produto.
+     */
+    public void preencherCampoQuantidadeEdicao(String quantidade) {
+        WebElement campoQuantidade = driver.findElement(By.id("edit-quantidade"));
+        campoQuantidade.clear();
+        campoQuantidade.sendKeys(quantidade);
+    }
+
+    /**
+     * Preenche o campo de valor do produto na tela de edição com o valor especificado.
+     *
+     * @param valor O novo valor do produto.
+     */
+
+    public void preencherCampoValorEdicao(String valor) {
+        WebElement campoValor = driver.findElement(By.id("edit-valor"));
+        campoValor.clear();
+        campoValor.sendKeys(valor);
+    }
+
+    /**
+     * Preenche o campo de data de cadastro do produto na tela de edição com o valor especificado.
+     *
+     * @param data A nova data de cadastro do produto.
+     */
+    public void preencherCampoDataCadastroEdicao(String data) {
+        WebElement campoDataCadastro = driver.findElement(By.id("edit-data"));
+        campoDataCadastro.clear();
+        campoDataCadastro.sendKeys(data);
+    }
+
+    /**
+     * Clica no botão "Salvar" na tela de edição.
+     */
+    public void clicarBotaoSalvarEdicao() {
+        WebElement buttonSalvar = driver.findElement(By.id("btn-edit-salvar"));
+        buttonSalvar.click();
+    }
+
+    /**
+     * Verifica se a tela de edição está aberta.
+     *
+     * @return true se a tela de edição estiver aberta, false caso contrário.
+     */
+    public boolean telaEdicaoEstaAberta() {
+        WebElement divEdicao = driver.findElement(By.id("edicao-produto"));
+        return divEdicao.isDisplayed();
+    }
+
+    /**
+     * Obtém o texto da mensagem de erro na tela de edição.
+     *
+     * @return O texto da mensagem de erro.
+     */
+    public String obterTextoMensagemErroEdicao() {
+        WebElement spamErroEdicao = driver.findElement(By.cssSelector("div.alert.alert-danger.edicao"));
+        return spamErroEdicao.getText();
+    }
+
+    /**
+     * Clica no botão de exclusão (ícone de lixeira) do produto com o nome especificado.
+     *
+     * @param nome O nome do produto a ser excluído.
+     */
+    public void clicarBotaoExclusaoProduto(String nome) {
+        List<WebElement> rows = tableProduto;
+        for (WebElement row : rows) {
+            List<WebElement> columns = row.findElements(By.tagName("td"));
+            if (!columns.isEmpty() && columns.get(1).getText().equals(nome)) {
+                WebElement buttonDelete = row.findElement(By.className("btn-delete"));
+                buttonDelete.click();
+                break;
+            }
+        }
+    }
+
+    /**
+     * Verifica se a tela de detalhes do produto está aberta.
+     *
+     * @return true se a tela estiver aberta, false caso contrário.
+     */
+    public boolean telaDetalhesEstaAberta() {
+        return divDetalhes.isDisplayed();
+    }
+
+    /**
+     * Clica no botão de visualização (ícone de olho) do produto com o nome especificado.
+     *
+     * @param nome O nome do produto a ser visualizado.
+     */
+    public void clicarBotaoVisualizacaoProduto(String nome) {
+        List<WebElement> rows = tableProduto;
+        for (WebElement row : rows) {
+            List<WebElement> cells = row.findElements(By.tagName("td"));
+            if (cells.get(1).getText().equals(nome)) {
+                cells.get(5).findElement(By.className("fa-eye")).click();
+                break;
+            }
+        }
+    }
+
+    /**
+     * Obtém o nome do produto exibido na tela de detalhes.
+     *
+     * @return O nome do produto.
+     */
+    public String obterNomeProdutoDetalhes() {
+        return spanNomeProdutoDetalhes.getText();
+    }
+
+    /**
+     * Obtém a quantidade do produto exibida na tela de detalhes.
+     *
+     * @return A quantidade do produto.
+     */
+    public String obterQuantidadeProdutoDetalhes() {
+        return spanQuantidadeProdutoDetalhes.getText();
+    }
+
+    /**
+     * Obtém o valor do produto exibido na tela de detalhes.
+     *
+     * @return O valor do produto.
+     */
+    public String obterValorProdutoDetalhes() {
+        return spanValorProdutoDetalhes.getText();
+    }
+
+    /**
+     * Obtém a data de cadastro do produto exibida na tela de detalhes.
+     *
+     * @return A data de cadastro do produto.
+     */
+    public String obterDataCadastroProdutoDetalhes() {
+        return spanDataCadastroProdutoDetalhes.getText();
+    }
 }
--- src/test/java/sistemadetestes/test/ProductTest.java
+++ src/test/java/sistemadetestes/test/ProductTest.java
@@ -5,6 +5,7 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import sistemadetestes.pageObject.LoginPO;
 import sistemadetestes.pageObject.ProductPO;
@@ -84,5 +85,158 @@ public class ProductTest extends BaseTest {
         productPage.clicarBotaoSalvar();
         productPage.fecharMensagemDeErro();
         assertFalse(productPage.mensagemDeErroEstaVisivel());
+    }
+
+    /**
+     * Teste para verificar se é possível editar um produto existente.
+     */
+    @Test
+    public void TC006_deveEditarUmProdutoExistente() {
+        // Selecionar um produto existente para editar
+        List<WebElement> productRows = productPage.obterLinhasProdutos();
+        if (productRows.size() > 0) {
+            WebElement produtoParaEditar = productRows.get(0);
+            produtoParaEditar.click();
+
+            // Realizar a edição do produto
+            String novoNome = "Novo Nome";
+            productPage.limparCampoNome();
+            productPage.preencherCampoNome(novoNome);
+            productPage.clicarBotaoSalvar();
+
+            // Verificar se a edição foi bem-sucedida
+            assertEquals(novoNome, produtoParaEditar.findElement(By.cssSelector("td:nth-child(2)")).getText());
+        }
+    }
+
+    /**
+     * Teste para verificar se é possível excluir um produto existente.
+     */
+    @Test
+    public void TC007_deveExcluirUmProdutoExistente() {
+        // Cria um produto para ser excluído
+        String codigo = "1";
+        String nomeProduto = "Produto Teste";
+        String quantidadeProduto = "10";
+        String valorProduto = "50.00";
+        String dataCadastroProduto = "05/06/2023";
+
+        // Clica no botão "Criar" para abrir a tela de cadastro de produtos
+//        productPage.clicarBotaoCriar();
+        productPage.clicarBotaoCriar();
+
+        // Preenche os campos de cadastro de produtos
+        productPage.preencherCampoCodigo(codigo);
+        productPage.preencherCampoNome(nomeProduto);
+        productPage.preencherCampoQuantidade(quantidadeProduto);
+        productPage.preencherCampoValor(valorProduto);
+        productPage.preencherCampoDataCadastro(dataCadastroProduto);
+
+        // Clica no botão "Salvar" para cadastrar o produto
+        productPage.clicarBotaoSalvar();
+
+        // Verifica se o produto foi cadastrado corretamente
+        assertTrue(productPage.verificarProdutoNaTabela(nomeProduto));
+
+        // Clica no botão de exclusão (ícone de lixeira) do produto cadastrado
+        productPage.clicarBotaoExclusaoProduto(nomeProduto);
+
+        // Verifica se o produto foi excluído corretamente
+        assertFalse(productPage.verificarProdutoNaTabela(nomeProduto));
+    }
+
+    /**
+     * Teste para verificar se é possível adicionar um novo produto.
+     */
+    @Test
+    public void TC008_deveAdicionarUmNovoProduto() {
+        // Dados do novo produto
+        String codigo = "1";
+        String nomeProduto = "Novo Produto";
+        String quantidadeProduto = "5";
+        String valorProduto = "100.00";
+        String dataCadastroProduto = "2023-06-23";
+
+
+        // Verifica se o produto não está presente na tabela antes de adicionar
+        assertFalse(productPage.verificarProdutoNaTabela(nomeProduto));
+
+        // Clica no botão "Criar" para abrir a tela de cadastro de produtos
+//        productPage.clicarBotaoCriar();
+        productPage.clicarBotaoCriar();
+
+        // Preenche os campos de cadastro de produtos
+        productPage.preencherCampoCodigo(codigo);
+        productPage.preencherCampoNome(nomeProduto);
+        productPage.preencherCampoQuantidade(quantidadeProduto);
+        productPage.preencherCampoValor(valorProduto);
+        productPage.preencherCampoDataCadastro(dataCadastroProduto);
+
+        // Clica no botão "Salvar" para cadastrar o produto
+        productPage.clicarBotaoSalvar();
+
+        // Verifica se o produto foi cadastrado corretamente
+        assertTrue(productPage.verificarProdutoNaTabela(nomeProduto));
+    }
+
+    /**
+     * Teste para verificar se a validação de campos obrigatórios funciona corretamente no cadastro de produtos.
+     */
+    @Test
+    public void TC009_deveValidarCamposObrigatoriosNoCadastroDeProdutos() {
+
+        // Clica no botão "Criar" para abrir a tela de cadastro de produtos
+//        productPage.clicarBotaoCriar();
+        productPage.clicarBotaoCriar();
+
+        // Clica no botão "Salvar" sem preencher os campos obrigatórios
+        productPage.clicarBotaoSalvar();
+
+        // Verifica se a mensagem de erro está visível
+        assertTrue(productPage.mensagemDeErroEstaVisivel());
+
+        // Fecha a mensagem de erro
+        productPage.fecharMensagemDeErro();
+
+        // Verifica se a tela de cadastro ainda está aberta
+        assertTrue(productPage.telaCadastroEstaAberta());
+    }
+
+    /**
+     * Teste para verificar se é possível visualizar os detalhes de um produto.
+     */
+    @Test
+    public void TC010_deveVisualizarOsDetalhesDeUmProduto() {
+// Clica no botão "Criar" para abrir a tela de cadastro de produtos
+        productPage.clicarBotaoCriar();
+
+        // Preenche os campos obrigatórios
+        String codigo = "1";
+        String nomeProduto = "Produto Teste";
+        String quantidadeProduto = "10";
+        String valorProduto = "50.00";
+        String dataCadastroProduto = "2023-06-23";
+
+        productPage.preencherCampoCodigo(codigo);
+        productPage.preencherCampoNome(nomeProduto);
+        productPage.preencherCampoQuantidade(quantidadeProduto);
+        productPage.preencherCampoValor(valorProduto);
+        productPage.preencherCampoDataCadastro(dataCadastroProduto);
+
+        // Clica no botão "Salvar" para cadastrar o produto
+        productPage.clicarBotaoSalvar();
+        productPage.clicarBotaoSalvar();
+
+        // Clica no botão de visualização (ícone de olho) do produto cadastrado
+        productPage.clicarBotaoVisualizacaoProduto(nomeProduto);
+
+        // Verifica se a tela de detalhes está aberta corretamente
+        assertTrue(productPage.telaDetalhesEstaAberta());
+
+        // Verifica se os detalhes do produto estão corretos
+        assertEquals(nomeProduto, productPage.obterNomeProdutoDetalhes());
+        assertEquals(quantidadeProduto, productPage.obterQuantidadeProdutoDetalhes());
+        assertEquals(valorProduto, productPage.obterValorProdutoDetalhes());
+        assertEquals(dataCadastroProduto, productPage.obterDataCadastroProdutoDetalhes());
     }
 }
